c3761f690d4f969cacab9f24d4b5693905c0b143
aleksejrevakin <alexey.amahasla@gmail.com>
2023-08-01 11:09:42 +0300

    Create project

---
+++ SurfTask/MainView/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  SurfTask
+//
+//  Created by Алексей Ревякин on 01.08.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
