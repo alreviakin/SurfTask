33feea23a8013aecd6644059627097fb99356b43
aleksejrevakin <alexey.amahasla@gmail.com>
2023-08-01 11:38:28 +0300

    Create scrollView and ImageView

--- SurfTask/MainView/MainViewController.swift
+++ SurfTask/MainView/MainViewController.swift
@@ -9,14 +9,52 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    //MARK: - UI Elements
+    private var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = UIColor(hexString: "#F3F3F5")
+        return scrollView
+    }()
+    
+    private var personImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 60
+        imageView.layer.masksToBounds = true
+        imageView.backgroundColor = .cyan
+        return imageView
+    }()
+    
+    //MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         configureation()
     }
     
+    //MARK: - Add views
     private func configureation() {
-        view.backgroundColor = UIColor(hexString: "#F3F3F5")
         title = "Профиль"
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(personImageView)
+    }
+    
+    //MARK: - Constraints
+    override func viewWillLayoutSubviews() {
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            personImageView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            personImageView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 24),
+            personImageView.widthAnchor.constraint(equalToConstant: 120),
+            personImageView.heightAnchor.constraint(equalToConstant: 120)
+        ])
     }
 
 }
